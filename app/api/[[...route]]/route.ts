06a53292f693bb83ae4b1db51997cf104aff9f6e
HardiB-Salih <hardib.salih@gmail.com>
2024-08-11 23:23:12 +0300

    set up drezzel

--- app/api/[[...route]]/route.ts
+++ app/api/[[...route]]/route.ts
@@ -1,26 +1,30 @@
-import { clerkMiddleware, getAuth } from "@hono/clerk-auth";
-
 import { Hono } from "hono";
 import { handle } from "hono/vercel";
-import { zValidator } from "@hono/zod-validator";
-import { z } from "zod";
+import accounts from "./accounts";
+import { HTTPException } from "hono/http-exception";
 
 export const runtime = "edge";
 
 const app = new Hono().basePath("/api");
 
-app.get("/hello", clerkMiddleware(), (c) => {
-  const auth = getAuth(c);
-  if (!auth?.userId) {
-    return c.json({
-      message: "You are not logged in.",
-    });
+// Handle errors
+app.onError((err, c) => {
+  if (err instanceof HTTPException) {
+    return err.getResponse();
   }
+
+  return c.json({ error: "Internal server error" });
+});
+
+const routes = app.route("/accounts", accounts);
+
+app.get("/hello", (c) => {
   return c.json({
     message: "Hello Next.js!",
-    userId: auth.userId,
   });
 });
 
 export const GET = handle(app);
 export const POST = handle(app);
+
+export type AppType = typeof routes;
